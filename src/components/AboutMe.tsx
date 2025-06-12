7f204e879672ee4c7265621b136eca755d6e6f5a
kavanapateel <kavanapateel31@gmail.com>
2025-06-12 12:21:15 +0530

    Remove unused images and update AboutMe component; replace profile image source and enhance content for better engagement and clarity.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,12 +5,24 @@ const AboutMe: React.FC = () => {
   return (
     <div className="about-me-container">
       <div className="profile-image-wrapper">
-        <img src={"./assets/profile.jpg"} alt="Profile" className="profile-image" />
+        <img src={"/profile.jpg"} alt="Profile" className="profile-image" />
       </div>
       <div className="about-me-content">
-        <h2>About Me</h2>
-        <p>Ausca ren a perscn preparring for software developm̃ent interviews.</p>
-        <p>In project experiencé ·em.</p>
+        <h3>Hello,</h3>
+        <h1>I'm <span>Kavana</span>.</h1>
+        <p>👋 Welcome to my portfolio!  <br />
+          I'm a passionate Software Developer, and this page is just a glimpse into my journey in tech.
+          Scroll through to explore some of the projects I've worked on — each one is a piece
+          of what I've learned and loved building. I hope you enjoy checking them out as much as I enjoyed creating them!
+        </p>
+        <p>📄 Want to see more?  
+          Click below to view or download my resume.
+        </p>
+        <button className="resume-button">
+          <a href="/Kavana_FullStack_Resume.pdf" target="_blank" rel="noopener noreferrer">
+            View Resume
+          </a>
+        </button>
       </div>
     </div>
   );

8e1304708ae4dbbe3a779c87a47e8d3859216a27
kavanapateel <kavanapateel31@gmail.com>
2025-06-12 13:28:18 +0530

    Enhance AboutMe component with new LinkedIn link and improved styling; increase profile image size, adjust layout gaps, and add resume button for better user engagement.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,21 +6,30 @@ const AboutMe: React.FC = () => {
     <div className="about-me-container">
       <div className="profile-image-wrapper">
         <img src={"/profile.jpg"} alt="Profile" className="profile-image" />
+        <a 
+          href="https://www.linkedin.com/in/kavana-31dec/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="linkedin-link"
+        >
+          View My LinkedIn Profile
+        </a>
       </div>
       <div className="about-me-content">
         <h3>Hello,</h3>
         <h1>I'm <span>Kavana</span>.</h1>
         <p>👋 Welcome to my portfolio!  <br />
-          I'm a passionate Software Developer, and this page is just a glimpse into my journey in tech.
-          Scroll through to explore some of the projects I've worked on — each one is a piece
-          of what I've learned and loved building. I hope you enjoy checking them out as much as I enjoyed creating them!
+          I'm a passionate Software Developer, and this page is just a glimpse into my journey.
+          Scroll through to explore some of the details, I've worked on — each one is a piece
+          of what I've learned and loved building. <br />
+          I hope you enjoy checking them out as much as I enjoyed creating them!
         </p>
-        <p>📄 Want to see more?  
+        <p>📄 Want to see more?  <br />
           Click below to view or download my resume.
         </p>
         <button className="resume-button">
-          <a href="/Kavana_FullStack_Resume.pdf" target="_blank" rel="noopener noreferrer">
-            View Resume
+          <a href="/Kavana_FullStack_Developer_Resume.pdf" target="_blank" rel="noopener noreferrer">
+            My Resume
           </a>
         </button>
       </div>
